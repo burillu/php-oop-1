7bd8a02f6027724ee21023751e04d87d414a3596
burillu <burillu90@hotmail.it>
2023-12-04 14:53:27 +0100

    scaffolding base project

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include __DIR__ . "/partials/header.php";
+
+?>
+
+<div class="container">
+  <h1>pippo</h1>
+  <?php echo $load ?>
+</div>
+
+<?php
+include __DIR__ . "/partials/footer.php";
+?>
