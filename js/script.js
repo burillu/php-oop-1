f2bc83a58505cfa26892e5b3b4610a3c25d0deb8
burillu <burillu90@hotmail.it>
2023-12-04 23:32:15 +0100

    mod sctipt => no Vue

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
 
-const { createApp } = Vue
+// const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    }
-  }).mount('#app')
+// createApp({
+//     data() {
+//         return {
+//             message: 'Hello Vue!'
+//         }
+//     }
+// }).mount('#app')
