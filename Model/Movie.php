1689ecf79f9f8c56f496e4f66715e31eb66259a0
burillu <burillu90@hotmail.it>
2023-12-04 15:35:58 +0100

    meth printCard in Movie ok

--- Model/Movie.php
+++ Model/Movie.php
@@ -21,9 +21,30 @@ class Movie
         $this->poster_path = $poster_path;
         $this->original_language = $original_language;
     }
+    public function printCard()
+    {
+        $image = $this->poster_path;
+        $title = $this->title;
+        $content = $this->overview;
+        $custom = $this->vote_average;
+        include __DIR__ . "/Card.php";
+    }
+
 }
 
-$Babylon = new Movie(9381, "Babylon A.D.", "A veteran-turned-mercenary is hired to take a young woman with a secret from post-apocalyptic Eastern Europe to New York City.", 5.601, "2008-08-20", "https://image.tmdb.org/t/p/w342/kt9nqD0uOar8IVE9191HXhWOXKI.jpg", "en");
-var_dump($Babylon);
+
+
+
+$movie_string = file_get_contents(__DIR__ . '/movie_db.json');
+$movieList = json_decode($movie_string, true);
+// $Babylon = new Movie($movieList[4]['id'], $movieList[4]['title'], $movieList[4]['overview'], $movieList[4]['vote_average'], $movieList[4]['release_date'], $movieList[4]['poster_path'], $movieList[4]['original_language']);
+// var_dump($Babylon);
+$movies = [];
+foreach ($movieList as $movie) {
+    $movies[] = new Movie($movie['id'], $movie['title'], $movie['overview'], $movie['vote_average'], $movie['release_date'], $movie['poster_path'], $movie['original_language']);
+}
+//var_dump($movies);
+
+
 
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 include __DIR__ . "/partials/header.php";
+include __DIR__ . "/Model/Movie.php";
 
 ?>
 
 <div class="container">
-  <h1>pippo</h1>
-  <?php echo $load ?>
+  <div class="row">
+    <?php foreach ($movies as $movie) {
+      echo $movie->printCard();
+    } ?>
+  </div>
+
 </div>
 
 <?php
