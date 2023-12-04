e58c8b54926ed7ceb77aa503df4737d98de9d680
burillu <burillu90@hotmail.it>
2023-12-04 23:33:17 +0100

    add stars template

--- Model/Vote.php
+++ Model/Vote.php
@@ -1,3 +1,5 @@
-<?php for ($i = 0; $i < $vote; $i++) {
-    echo "<i class= 'fa-star fa-solid' ></i>";
+<?php
+$template = "";
+for ($i = 0; $i < $vote; $i++) {
+    $template .= "<i class= 'fa-star fa-solid' ></i>";
 } ?>
